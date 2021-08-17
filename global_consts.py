91f3cf19233186791cbccd60a2a1d1c54c087410
CdrSonan <64283097+CdrSonan@users.noreply.github.com>
2021-08-17 09:18:04 +0200

    added files for locale and global constant system

---
+++ global_consts.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 17 08:47:26 2021
+
+@author: CdrSonan
+"""
+
+sampleRate = 48000
+tickRate = 250
+batchSize = 192
+tripleBatchSize = 576
---
+++ locale.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 17 08:59:27 2021
+
+@author: CdrSonan
+"""
+
+en = dict()
+
+jp = dict()
+
+tl = dict()
+
+de = dict()
