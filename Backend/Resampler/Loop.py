fa2d1bca400d75f0decb4715c84e0261fcd0d56e
CdrSonan <64283097+CdrSonan@users.noreply.github.com>
2021-10-06 02:07:34 +0200

    some groundwork for tomorrow

--- Backend/Resampler/Loop.py
+++ Backend/Resampler/Loop.py
@@ -7,27 +7,32 @@ phaseShift = Backend.Resampler.PhaseShift.phaseShift
 def loopSamplerVoicedExcitation(inputTensor, targetSize, repetititionSpacing, pitch, device):
     inputTensor = inputTensor.to(device = device)
     repetititionSpacing = repetititionSpacing.to(device = device)
-    batchRS = math.ceil(repetititionSpacing * inputTensor.size()[0] / 2)
-    repetititionSpacing = int(repetititionSpacing * math.ceil(inputTensor.size()[0] / 2))
+    batchRS = math.ceil(repetititionSpacing * inputTensor.size()[1] / 2)
+    repetititionSpacing = int(repetititionSpacing * global_consts.batchSize * math.ceil(inputTensor.size()[1] / 2))
     window = torch.hann_window(global_consts.tripleBatchSize, device = device)
     alignPhase = inputTensor[batchRS][pitch].angle()
     finalPhase = inputTensor[1][pitch].angle()
     phaseDiff = (finalPhase - alignPhase)
-    requiredTensors = max(math.ceil((targetSize/global_consts.batchSize - batchRS) / (inputTensor.size()[1] - math.ceil(global_consts.tripleBatchSize / pitch) - batchRS)), 1)
+    requiredTensors = max(math.ceil((targetSize/global_consts.batchSize - batchRS) / (inputTensor.size()[1] - math.ceil(global_consts.tripleBatchSize / pitch) - batchRS)), 1) #!!!BUGGED!!!
+
+    #fix insertion timings, include modulo operations
+    #pre-calculate istft
+    #adjust repetitition spacing to only cover 1/2 of the shortest possible sample
+
+
     if requiredTensors <= 1:
         outputTensor = inputTensor.clone()
-        #outputTensor = torch.istft(outputTensor, global_consts.tripleBatchSize, hop_length = global_consts.batchSize, win_length = global_consts.tripleBatchSize, window = window, onesided = True, length = inputTensor.size()[1]*global_consts.batchSize)
+        outputTensor = torch.istft(outputTensor, global_consts.tripleBatchSize, hop_length = global_consts.batchSize, win_length = global_consts.tripleBatchSize, window = window, onesided = True, length = inputTensor.size()[1]*global_consts.batchSize)
     else:
         outputTensor = torch.zeros(requiredTensors * (inputTensor.size()[1] * global_consts.batchSize - repetititionSpacing) + repetititionSpacing, device = device)
-            
         workingTensor = inputTensor.clone()
-        #workingTensor = torch.istft(workingTensor, global_consts.tripleBatchSize, hop_length = global_consts.batchSize, win_length = global_consts.tripleBatchSize, window = window, onesided = True, length = inputTensor.size()[1]*global_consts.batchSize)
+        workingTensor = torch.istft(workingTensor, global_consts.tripleBatchSize, hop_length = global_consts.batchSize, win_length = global_consts.tripleBatchSize, window = window, onesided = True, length = inputTensor.size()[1]*global_consts.batchSize)
         workingTensor[-repetititionSpacing:] = workingTensor[-repetititionSpacing:] * torch.linspace(1, 0, repetititionSpacing, device = device)
         outputTensor[0:inputTensor.size()[1] * global_consts.batchSize] += workingTensor
 
         for i in range(1, requiredTensors - 1):
             workingTensor = inputTensor.clone()
-            #workingTensor = torch.istft(workingTensor, global_consts.tripleBatchSize, hop_length = global_consts.batchSize, win_length = global_consts.tripleBatchSize, window = window, onesided = True, length = inputTensor.size()[1]*global_consts.batchSize)
+            workingTensor = torch.istft(workingTensor, global_consts.tripleBatchSize, hop_length = global_consts.batchSize, win_length = global_consts.tripleBatchSize, window = window, onesided = True, length = inputTensor.size()[1]*global_consts.batchSize)
             #workingTensor = phaseShift(workingTensor, pitch, i * phaseDiff, device = device)
             phaseOffset = (global_consts.tripleBatchSize * 0.5 / math.pi) * (torch.remainder(i * phaseDiff, 2 * math.pi) / pitch)
             workingTensor = workingTensor[phaseOffset:]
@@ -36,7 +41,7 @@ def loopSamplerVoicedExcitation(inputTensor, targetSize, repetititionSpacing, pi
             outputTensor[i * (inputTensor.size()[1] * global_consts.batchSize - repetititionSpacing):i * (inputTensor.size()[1] * global_consts.batchSize - repetititionSpacing) + inputTensor.size()[1] * global_consts.batchSize] += workingTensor #account for shrinking
 
         workingTensor = inputTensor.clone()
-        #workingTensor = torch.istft(workingTensor, global_consts.tripleBatchSize, hop_length = global_consts.batchSize, win_length = global_consts.tripleBatchSize, window = window, onesided = True, length = inputTensor.size()[1]*global_consts.batchSize)
+        workingTensor = torch.istft(workingTensor, global_consts.tripleBatchSize, hop_length = global_consts.batchSize, win_length = global_consts.tripleBatchSize, window = window, onesided = True, length = inputTensor.size()[1]*global_consts.batchSize)
         #workingTensor = phaseShift(workingTensor, pitch, (requiredTensors - 1) * phaseDiff, device = device)
         phaseOffset = (global_consts.tripleBatchSize * 0.5 / math.pi) * (torch.remainder((requiredTensors - 1) * phaseDiff, 2 * math.pi) / pitch)
         workingTensor = workingTensor[phaseOffset:]
--- z_test.py
+++ z_test.py
@@ -4,16 +4,21 @@ import global_consts
 from Backend.Resampler.Loop import loopSamplerVoicedExcitation as shift
 from math import sin, pi
 
-print(torch.linspace(0, global_consts.halfTripleBatchSize + 1, global_consts.halfTripleBatchSize + 1))
+window = torch.hann_window(global_consts.tripleBatchSize)
 
-data = torch.empty(global_consts.tripleBatchSize)
-for i in range(global_consts.tripleBatchSize):
+data = torch.empty(10 * global_consts.tripleBatchSize)
+for i in range(10 * global_consts.tripleBatchSize):
     data[i] = sin(i * pi / 128)
     data[i] += 0.1 * sin(i * pi /32)
 
-dataShift = shift(data, 2 * global_consts.tripleBatchSize, torch.tensor([0.5]), 1, None)
 print(data.size())
-print(dataShift.size())
 plt.plot(data)
+
+data = torch.stft(data, global_consts.tripleBatchSize, hop_length = global_consts.batchSize, win_length = global_consts.tripleBatchSize, window = window, return_complex = True, onesided = True)
+
+dataShift = shift(data, 40 * global_consts.tripleBatchSize, torch.tensor([0.5]), 1, None)
+
+print(dataShift.size())
 plt.plot(dataShift)
+
 plt.show()
