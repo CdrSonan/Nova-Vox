a5638d1c5a826e44d1a37a5c007e9c117dd8be03
CdrSonan <64283097+CdrSonan@users.noreply.github.com>
2021-10-22 23:38:26 +0200

    misc. bugfixes

--- z_test copy.py
+++ z_test copy.py
@@ -1,14 +1,86 @@
 import csv
 import torch
-import global_consts
-from Backend.VB_Components.SpecCrfAi import SpecCrfAi
+import numpy
+from Backend.VB_Components.SpecCrfAi import RelLoss, SpecCrfAi
+
+hiddenLayerNumberMax = 10
 
 data = torch.load("data.dat")
 testVBNumber = int(input("test Voicebank number? >>>"))
-testSampleBatchNumber = int(input("test sample batch? >>>"))
-
-testVB = data.pop(testVBNumber)
-dataTmp = data[testVBNumber + 1]
-testSampleBatch = dataTmp.pop(testSampleBatchNumber)
-data[testVBNumber + 1] = dataTmp
-
+hiddenLayerNumber = int(input("hidden layer number start? >>>"))
+device = input("training device? >>>")
+if device == "GPU":
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+testData = data.pop(testVBNumber)
+trainData = data.pop(0)
+for i in range(len(data)):
+    trainData = torch.cat(trainData, data.pop(0), 0)
+trainData = trainData.to(device)
+testData = testData.to(device)
+for i in range(hiddenLayerNumber, hiddenLayerNumberMax):
+    crfAi = SpecCrfAi(device, hiddenLayerCount = i)
+    crfAi.train(trainData)
+    trainResults = []
+    testResults = []
+    for j in trainData:
+        encodedTypes = j[1]
+        j = j[0]
+        spectrum1 = j[0]
+        spectrum2 = j[1]
+        spectrum3 = j[-2]
+        spectrum4 = j[-1]
+        if encodedTypes == 0:
+            type1 = "V"
+            type2 = "V"
+        elif encodedTypes == 1:
+            type1 = "V"
+            type2 = "C"
+        elif encodedTypes == 2:
+            type1 = "V"
+            type2 = "c"
+        elif encodedTypes == 3:
+            type1 = "C"
+            type2 = "V"
+        elif encodedTypes == 4:
+            type1 = "C"
+            type2 = "C"
+        elif encodedTypes == 5:
+            type1 = "C"
+            type2 = "c"
+        elif encodedTypes == 6:
+            type1 = "c"
+            type2 = "V"
+        elif encodedTypes == 7:
+            type1 = "c"
+            type2 = "C"
+        elif encodedTypes == 8:
+            type1 = "c"
+            type2 = "c"
+        for k in range(j.size()[0]):
+            factor = i / float(j.size()[0])
+            spectrumTarget = j[k]
+            output = torch.squeeze(crfAi.processData(spectrum1, spectrum2, spectrum3, spectrum4, factor, type1, type2))
+            loss = RelLoss(output, spectrumTarget)
+            trainResults.append(loss)
+    trainAvg = numpy.mean(trainResults)
+    trainStd = numpy.std(trainResults)
+    del trainResults
+    for j in testData:
+        spectrum1 = j[0]
+        spectrum2 = j[1]
+        spectrum3 = j[-2]
+        spectrum4 = j[-1]
+        for k in range(j.size()[0]):
+            factor = i / float(j.size()[0])
+            spectrumTarget = j[k]
+            output = torch.squeeze(crfAi.processData(spectrum1, spectrum2, spectrum3, spectrum4, factor, type1, type2))
+            loss = RelLoss(output, spectrumTarget)
+            testResults.append(loss)
+    testAvg = numpy.mean(testResults)
+    testStd = numpy.std(testResults)
+    del testResults
+    writer = csv.writer(open("results.csv", "w"))
+    writer.writerow([testVBNumber, hiddenLayerNumber, testAvg, testStd])
+    writer.close()
