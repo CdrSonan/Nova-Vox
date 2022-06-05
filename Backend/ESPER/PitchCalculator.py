spectral calculation and misc fixes

Backend/ESPER/PitchCalculator.py
@@ -62,6 +62,6 @@ def calculatePitch(audioSample):
     except Exception as e:
         print("error during pitch detection; falling back to default values", e)
         audioSample.pitchDeltas = torch.full([ceil(audioSample.waveform.size()[0] / global_consts.batchSize),], audioSample.expectedPitch)
-    print("pitch: ", audioSample.pitchDeltas)
+    #print("pitch: ", audioSample.pitchDeltas)
     audioSample.pitch = torch.mean(audioSample.pitchDeltas).int()
     audioSample.pitchDeltas = audioSample.pitchDeltas.to(torch.int16)

Backend/ESPER/SpectralCalculator.py
@@ -15,16 +15,14 @@ def calculateSpectra(audioSample):
     The algorithm first runs an amount of filtering iterations determined by voicedIterations, selectively saves the peaking frequencies of the signal into _voicedExcitations, 
     then runs the filtering algorithm again a number of iterations determined by unvoicedIterations.
     The function fills the spectrum, spectra and _voicedExcitations properties."""
-    SiLU = torch.nn.SiLU()# softplus?
     threshold = torch.nn.Threshold(0.001, 0.001)
-    #perhaps lower FilterTEEMult for voiced/unvoiced separation and increase UI filter value instead
     window = torch.hann_window(global_consts.tripleBatchSize * global_consts.filterBSMult)
-    #spectralFilterWidth = torch.max(torch.floor(global_consts.tripleBatchSize * global_consts.filterBSMult * global_consts.filterTEEMult / audioSample.pitch), torch.Tensor([1])).int().item()
     signals = torch.stft(audioSample.waveform, global_consts.tripleBatchSize * global_consts.filterBSMult, hop_length = global_consts.batchSize * global_consts.filterBSMult, win_length = global_consts.tripleBatchSize * global_consts.filterBSMult, window = window, return_complex = True, onesided = True)
     signals = torch.transpose(signals, 0, 1)
     signalsAbs = signals.abs()
     
-    spectralFilterWidth = torch.max(torch.floor(global_consts.tripleBatchSize * global_consts.filterBSMult / audioSample.pitch), torch.Tensor([1])).int().item()
+    #spectrum approximation for voiced/unvoiced separation
+    spectralFilterWidth = torch.max(torch.floor(audioSample.pitch / global_consts.tripleBatchSize * global_consts.filterBSMult * global_consts.filterHRSSMult), torch.Tensor([1])).int().item()
     workingSpectra = torch.sqrt(signalsAbs)
     audioSample.spectra = workingSpectra.clone()
     for j in range(audioSample.voicedFilter):
@@ -34,9 +32,10 @@ def calculateSpectra(audioSample):
         workingSpectra = torch.min(workingSpectra, audioSample.spectra)
         audioSample.spectra = workingSpectra
     
+    #resonance calculations
     resonanceFunction = torch.zeros_like(audioSample.spectra)
     for i in range(resonanceFunction.size()[0]):
-        #sin(2*pi*a)/(a^2-1) mit a = f/f0 = l0/l
+        #sin(2*pi*a)/(a^2-1) with a = f/f0 = l0/l
         for j in range(global_consts.nFormants):
             freqspace = torch.linspace(0, audioSample.pitch / (j + 1), global_consts.halfTripleBatchSize * global_consts.filterBSMult + 1)
             freqspace = torch.sin(2 * pi * freqspace) / (torch.pow(freqspace, torch.full([1,], 2.)) - torch.ones([1,]))
@@ -47,6 +46,7 @@ def calculateSpectra(audioSample):
         resonanceFunction[i][transitionPoint - global_consts.nFormants:transitionPoint] += torch.linspace(0, 1, global_consts.nFormants)
         resonanceFunction[i][transitionPoint:] = torch.ones_like(resonanceFunction[i][transitionPoint:])
 
+    #phase continuity calculations
     phaseContinuity = torch.empty_like(signals, dtype = torch.float32)
     for i in range(phaseContinuity.size()[0] - 1):
         phaseContinuity[i] = signals[i].angle() - signals[i + 1].angle()
@@ -57,16 +57,18 @@ def calculateSpectra(audioSample):
     phaseContinuity += pi
     phaseContinuity /= pi
 
-    audioSample._voicedExcitations = signals.clone()
-    audioSample._voicedExcitations *= torch.gt(signalsAbs * resonanceFunction * (1. - phaseContinuity), audioSample.spectra * audioSample.voicedFilter)
+    #voiced/unvoiced separation
+    audioSample.voicedExcitation = signals.clone()
+    audioSample.voicedExcitation *= torch.gt(signalsAbs * resonanceFunction * (1. - 0.2 * torch.pow(phaseContinuity, torch.tensor([2.,]))), audioSample.spectra * audioSample.voicedFilter)
+    audioSample.excitation = signals.clone()
+    audioSample.excitation *= torch.less_equal(signalsAbs * resonanceFunction * (1. - 0.2 * torch.pow(phaseContinuity, torch.tensor([2.,]))), audioSample.spectra * audioSample.voicedFilter)
     if audioSample.isVoiced == False:
-        audioSample._voicedExcitations *= 0
+        audioSample.voicedExcitation *= 0
 
-    excitationAbs = signalsAbs
-    voicedExcitationAbs = audioSample._voicedExcitations.abs()
-    audioSample.excitation = torch.transpose(torch.sqrt(signals) * (excitationAbs - voicedExcitationAbs), 0, 1)
-    audioSample.voicedExcitation = torch.transpose(audioSample._voicedExcitations, 0, 1)
+    audioSample.voicedExcitation = torch.transpose(audioSample.voicedExcitation, 0, 1)
+    audioSample.excitation = torch.transpose(audioSample.excitation, 0, 1)
 
+    #fourier space transforms
     audioSample.excitation = torch.istft(audioSample.excitation, global_consts.tripleBatchSize * global_consts.filterBSMult, hop_length = global_consts.batchSize * global_consts.filterBSMult, win_length = global_consts.tripleBatchSize * global_consts.filterBSMult, window = window, onesided = True)
     audioSample.voicedExcitation = torch.istft(audioSample.voicedExcitation, global_consts.tripleBatchSize * global_consts.filterBSMult, hop_length = global_consts.batchSize * global_consts.filterBSMult, win_length = global_consts.tripleBatchSize * global_consts.filterBSMult, window = window, onesided = True)
 
@@ -75,13 +77,16 @@ def calculateSpectra(audioSample):
     audioSample.excitation = torch.stft(audioSample.excitation, global_consts.tripleBatchSize, hop_length = global_consts.batchSize, win_length = global_consts.tripleBatchSize, window = window, return_complex = True, onesided = True)
     audioSample.voicedExcitation = torch.stft(audioSample.voicedExcitation, global_consts.tripleBatchSize, hop_length = global_consts.batchSize, win_length = global_consts.tripleBatchSize, window = window, return_complex = True, onesided = True)
 
-    spectralFilterWidth = torch.max(torch.floor(global_consts.tripleBatchSize * global_consts.filterTEEMult / audioSample.pitch), torch.Tensor([1])).int().item()
-
-    signals = torch.stft(audioSample.waveform, global_consts.tripleBatchSize, hop_length = global_consts.batchSize, win_length = global_consts.tripleBatchSize, window = window, return_complex = True, onesided = True)
+    #signals = torch.stft(audioSample.waveform, global_consts.tripleBatchSize, hop_length = global_consts.batchSize, win_length = global_consts.tripleBatchSize, window = window, return_complex = True, onesided = True)
+    signals = audioSample.excitation.abs() + audioSample.voicedExcitation.abs()
     signals = torch.transpose(signals, 0, 1)
-    signalsAbs = signals.abs()
+    #signalsAbs = signals.abs()
+    signalsAbs = signals
     signalsAbs = torch.sqrt(signalsAbs)
+
+    #low-range quefrency-space lowpass smoothing
     audioSample.spectra = signalsAbs.clone()
+    spectralFilterWidth = torch.max(torch.floor(global_consts.tripleBatchSize * global_consts.filterTEEMult / audioSample.pitch), torch.Tensor([1])).int().item()
     spectralFilterWidth = min(spectralFilterWidth, floor(audioSample.spectra.size()[1] / 2))
     for j in range(audioSample.unvoicedIterations):
         audioSample.spectra = torch.maximum(audioSample.spectra, signalsAbs)
@@ -90,10 +95,9 @@ def calculateSpectra(audioSample):
         cutoffWindow[0:int(spectralFilterWidth / 2)] = 1.
         cutoffWindow[int(spectralFilterWidth / 2):spectralFilterWidth] = torch.linspace(1, 0, spectralFilterWidth - int(spectralFilterWidth / 2))
         audioSample.spectra = torch.fft.irfft(cutoffWindow * audioSample.spectra, dim = 1, n = global_consts.halfTripleBatchSize + 1)
-    audioSample.spectra = threshold(audioSample.spectra)
 
-
-    spectralFilterWidth = torch.max(torch.floor(global_consts.tripleBatchSize / audioSample.pitch), torch.Tensor([1])).int()
+    #high-range frequency-space running mean smoothing
+    spectralFilterWidth = torch.max(torch.floor(audioSample.pitch / global_consts.tripleBatchSize * global_consts.filterHRSSMult), torch.Tensor([1])).int()
     workingSpectra = signalsAbs.clone()
     workingSpectra = torch.cat((workingSpectra, torch.tile(torch.unsqueeze(workingSpectra[:, -1], 1), (1, audioSample.unvoicedIterations))), 1)
     spectra = workingSpectra.clone()
@@ -104,11 +108,14 @@ def calculateSpectra(audioSample):
         workingSpectra = torch.max(workingSpectra, spectra)
         spectra = workingSpectra
     spectra = spectra[:, 0:global_consts.halfTripleBatchSize + 1]
+
+    #finalisation
     slope = torch.ones_like(spectra)
     slope[:, global_consts.spectralRolloff2:] = 0.
     slope[:, global_consts.spectralRolloff1:global_consts.spectralRolloff2] = torch.linspace(1, 0, global_consts.spectralRolloff2 - global_consts.spectralRolloff1)
+    audioSample.spectra = threshold(audioSample.spectra)
+    spectra = threshold(spectra)
     audioSample.spectra = slope * audioSample.spectra + ((1. - slope) * spectra)
-
 
     audioSample.spectrum = torch.mean(audioSample.spectra, 0)
     for i in range(audioSample.spectra.size()[0]):
@@ -117,8 +124,11 @@ def calculateSpectra(audioSample):
     audioSample.voicedExcitation = audioSample.voicedExcitation / torch.transpose(torch.square(audioSample.spectrum + audioSample.spectra)[0:audioSample.voicedExcitation.size()[1]], 0, 1)
     audioSample.excitation = torch.transpose(audioSample.excitation, 0, 1) / torch.square(audioSample.spectrum + audioSample.spectra)[0:audioSample.excitation.size()[1]]
 
+    print("means: ", torch.mean(audioSample.voicedExcitation.abs()), torch.mean(audioSample.excitation.abs()))
+
     audioSample.voicedExcitation = torch.istft(audioSample.voicedExcitation, global_consts.tripleBatchSize, hop_length = global_consts.batchSize, win_length = global_consts.tripleBatchSize, window = window, onesided = True)
 
+    #phase calculations
     audioSample.phases = torch.empty_like(audioSample.pitchDeltas)
     """
         for i in range(audioSample.pitchDeltas.size()[0]):