41947e40988790641811fa2294a998f61f8fc4ed
CdrSonan <64283097+CdrSonan@users.noreply.github.com>
2023-03-04 00:35:24 +0100

    Create fft.pyx

---
+++ cython/fft.pyx
@@ -0,0 +1,87 @@
+# Copyright 2023 Contributors to the Nova-Vox project
+
+# This file is part of Nova-Vox.
+# Nova-Vox is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+# Nova-Vox is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with Nova-Vox. If not, see <https://www.gnu.org/licenses/>.
+
+from cython.cimports.libc.math import sin, cos, pi, exp, ceil
+
+cdef dft(float[:, 2] sequence):
+    cdef Py_ssize_t length = sequence.shape[0]
+    cdef float[length, 2] result
+    cdef float sine
+    cdef float cosine
+    for i in range(length):
+        result[i, 0] = 0
+        result[i, 1] = 1
+        for j in range(length):
+            sine = sin(2 * pi * i * j / length)
+            cosine = cos(2 * pi * i * j / length)
+            result[i, 0] += sequence[j, 0] * cosine
+            result[i, 0] += sequence[j, 1] * sine
+            result[i, 1] -= sequence[j, 0] * sine
+            result[i, 1] += sequence[j, 1] * cosine
+    return result
+
+cpdef fft(float[:, 2] sequence):
+    cdef Py_ssize_t length = sequence.shape[0]
+    cdef float[length, 2] result
+    cdef Py_ssize_t halfLen = length / 2
+    cdef float[halfLen, 2] inputBuffer
+    cdef float[halfLen, 2] outputBuffer
+    cdef float[2] exponentials
+    if length % 2 != 0:
+        return dft(sequence)
+    for i in range(halfLen):
+        inputBuffer[i] = sequence[2 * i]
+    outputBuffer = fft(inputBuffer)
+    for i in range(halfLen):
+        result[i] = outputBuffer[i]
+        inputBuffer[i] = sequence[2 * i + 1]
+    outputBuffer = fft(inputBuffer)
+    for i in range(halfLen):
+        exponentials[0] = cos(2 * pi * i / length)
+        exponentials[1] = -sin(2 * pi * i / length)
+        outputBuffer[i, 0] *= exponentials[0]
+        outputBuffer[i, 1] *= exponentials[1]
+        result[i + halfLen, 0] = result[i, 0] - outputBuffer[i, 0]
+        result[i + halfLen, 1] = result[i, 1] - outputBuffer[i, 1]
+        result[i, 0] += outputBuffer[i, 0]
+        result[i, 1] += outputBuffer[i, 1]
+    return result
+
+
+cdef rdft(float[:] sequence):
+    cdef Py_ssize_t length = sequence.shape[0]
+    cdef Py_ssize_t halfLen = ceil(length / 2) + 1
+    cdef float[halfLen, 2] result
+    cdef float sine
+    cdef float cosine
+    for i in range(halfLen):
+        result[i] = 0
+        for j in range(length):
+            sine = sin(2 * pi * i * j / length)
+            cosine = cos(2 * pi * i * j / length)
+            result[i, 0] += sequence[j] * cosine
+            result[i, 1] -= sequence[j] * sine
+    return result
+
+cpdef rfft(float[:] sequence):
+    cdef Py_ssize_t length = sequence.shape[0]
+    cdef Py_ssize_t halfLen = ceil(length / 2) + 1
+    cdef float[length, 2] compSeq
+    cdef float[halfLen, 2] result
+    if length % 4 != 0:
+        return rdft(sequence)
+    for i in range(length):
+        compSeq[i, 0] = sequence[i]
+        compSeq[i, 1] = 0
+    compSeq = fft(compSeq)
+    for i in range(halfLen):
+        result[i, 0] = compSeq[i, 0]
+        result[i, 1] = compSeq[i, 1]
+    return result
+
+def stft(float[:, 2] sequence, bool windowing, int winSize, int overlap, bool padding):
+    if padding:
