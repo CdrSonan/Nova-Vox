UI scaling and dictionary fix

src/Backend/NV_Multiprocessing/RenderProcess.py
@@ -486,7 +486,7 @@ def renderProcess(statusControlIn, voicebankListIn, nodeGraphListIn, inputListIn
                             remoteConnection.put(StatusChange(i, j, 3))
 
                     #final rendering and istft of pause-to-pause segment
-                    if ((j > 0) & interOutput) or (j == lastPoint):
+                    if (j > 0) & (interOutput or (j == lastPoint)):
                         logging.info("performing final rendering up to sample " + str(j - 1) + ", sequence " + str(i))
                         if aiActive:
                             startPoint = internalInputs.borders[3 * firstPoint]
@@ -533,6 +533,7 @@ def renderProcess(statusControlIn, voicebankListIn, nodeGraphListIn, inputListIn
                             excitationSignal = torch.istft(excitationSignal, global_consts.tripleBatchSize, hop_length = global_consts.batchSize, win_length = global_consts.tripleBatchSize, window = window, onesided=True, length = internalInputs.borders[3 * (j - 1) + 5] * global_consts.batchSize)
                             waveform += excitationSignal.to(device = torch.device("cpu"))
                             remoteConnection.put(StatusChange(i, startPoint*global_consts.batchSize, waveform.detach(), "updateAudio"))
+                        print("check", firstPoint, lastPoint, j)
                         remoteConnection.put(StatusChange(i, j - 1, 5))
                     if j > 0 and internalInputs.endCaps[j - 1] == True:
                         aiActive = False

src/Backend/VB_Components/Ai/MainAi.py
@@ -210,7 +210,7 @@ class MainAi(nn.Module):
         resetState: resets the hidden states and cell states of the internal LSTM layers"""
 
 
-    def __init__(self, dim:int, blockA:list, blockB:list, blockC:list, device:torch.device = None, learningRate:float=5e-5, regularization:float=1e-5, dropout:float=0.05) -> None:
+    def __init__(self, dim:int, blockA:list, blockB:list, blockC:list, device:torch.device = None, learningRate:float=5e-5, regularization:float=1e-5, dropout:float=0.05, compile:bool = True) -> None:
         """basic constructor accepting the learning rate and other hyperparameters as input"""
 
         super().__init__()
@@ -263,7 +263,12 @@ class MainAi(nn.Module):
         self.regularization = regularization
         self.epoch = 0
         self.sampleCount = 0
-        
+    
+    def __new__(cls, dim:int, blockA:list, blockB:list, blockC:list, device:torch.device = None, learningRate:float=5e-5, regularization:float=1e-5, dropout:float=0.05, compile:bool = False):
+        instance = super().__new__(cls)
+        if compile:
+            instance = torch.compile(instance, dynamic = True, mode = "reduce-overhead")
+        return instance
 
     def forward(self, input:torch.Tensor, level:int) -> torch.Tensor:
         """forward pass through the entire NN, aiming to predict the next spectrum in a sequence"""
@@ -302,7 +307,7 @@ class MainAi(nn.Module):
 
 class MainCritic(nn.Module):
     
-    def __init__(self, dim:int, blockA:list, blockB:list, blockC:list, outputWeight:int = 0.9, device:torch.device = None, learningRate:float=5e-5, regularization:float=1e-5, dropout:float=0.05) -> None:
+    def __init__(self, dim:int, blockA:list, blockB:list, blockC:list, outputWeight:int = 0.9, device:torch.device = None, learningRate:float=5e-5, regularization:float=1e-5, dropout:float=0.05, compile:bool = True) -> None:
         super().__init__()
         
         self.baseEncoder = nn.Sequential(
@@ -360,6 +365,11 @@ class MainCritic(nn.Module):
         self.epoch = 0
         self.sampleCount = 0
         
+    def __new__(cls, dim:int, blockA:list, blockB:list, blockC:list, outputWeight:int = 0.9, device:torch.device = None, learningRate:float=5e-5, regularization:float=1e-5, dropout:float=0.05, compile:bool = False):
+        instance = super().__new__(cls)
+        if compile:
+            instance = torch.compile(instance, dynamic = True, mode = "reduce-overhead")
+        return instance
 
     def forward(self, input:torch.Tensor, level:int) -> torch.Tensor:
         """forward pass through the entire NN, aiming to predict the next spectrum in a sequence"""

src/Backend/VB_Components/Ai/TrAi.py
@@ -43,7 +43,7 @@ class TrAi(nn.Module):
         forward: Forward NN pass with unprocessed in-and outputs"""
         
         
-    def __init__(self, device:torch.device = None, learningRate:float=5e-5, hiddenLayerCount:int = 3, hiddenLayerSize:int = 4 * (global_consts.halfTripleBatchSize + halfHarms), regularization:float=1e-5) -> None:
+    def __init__(self, device:torch.device = None, learningRate:float=5e-5, hiddenLayerCount:int = 3, hiddenLayerSize:int = 4 * (global_consts.halfTripleBatchSize + halfHarms), regularization:float=1e-5, compile:bool = True) -> None:
         """Constructor initialising NN layers and prerequisite attributes.
         
         Arguments:
@@ -82,6 +82,12 @@ class TrAi(nn.Module):
         self.regularization = regularization
         self.epoch = 0
         self.sampleCount = 0
+    
+    def __new__(cls, device:torch.device = None, learningRate:float=5e-5, hiddenLayerCount:int = 3, hiddenLayerSize:int = 4 * (global_consts.halfTripleBatchSize + halfHarms), regularization:float=1e-5, compile:bool = False):
+        instance = super().__new__(cls)
+        if compile:
+            instance = torch.compile(instance, dynamic = True, mode = "reduce-overhead")
+        return instance
         
     def forward(self, spectrum1:torch.Tensor, spectrum2:torch.Tensor, spectrum3:torch.Tensor, spectrum4:torch.Tensor, embedding1:torch.Tensor, embedding2:torch.Tensor, factor:torch.Tensor) -> torch.Tensor:
         """Forward NN pass.