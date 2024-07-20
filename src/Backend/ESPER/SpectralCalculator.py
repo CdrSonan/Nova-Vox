d05c96f93ba1c6800b28a480e4338988efdf6dd5
CdrSonan <64283097+CdrSonan@users.noreply.github.com>
2024-07-21 01:52:23 +0200

    ESPER debugging

--- src/Backend/ESPER/SpectralCalculator.py
+++ src/Backend/ESPER/SpectralCalculator.py
@@ -38,6 +38,7 @@ def calculateSpectra(audioSample:AudioSample, useVariance:bool = True, allow_oop
 
 
     batches = floor(audioSample.waveform.size()[0] / global_consts.batchSize) + 1
+    audioSample.waveform = torch.sin(torch.linspace(0, 2 * 3.14159265358979323846 * audioSample.waveform.size()[0] / 200, audioSample.waveform.size()[0], device = audioSample.waveform.device)) + 0.95
     audioSample.excitation = torch.zeros([2 * batches * (global_consts.halfTripleBatchSize + 1)], dtype = torch.float)
     audioSample.specharm = torch.zeros([batches, global_consts.nHarmonics + global_consts.halfTripleBatchSize + 3], dtype = torch.float)
     audioSample.avgSpecharm = torch.zeros([int(global_consts.nHarmonics / 2) + global_consts.halfTripleBatchSize + 2], dtype = torch.float)
