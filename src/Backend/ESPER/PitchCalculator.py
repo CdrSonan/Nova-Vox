pitch calculation fixes

src/Backend/ESPER/PitchCalculator.py
@@ -70,8 +70,8 @@ def calculatePitch(audioSample:AudioSample) -> None:
     audioSample.excitation = torch.zeros([2 * batches * (global_consts.halfTripleBatchSize + 1)], dtype = torch.float)
     cSample = C_Bridge.makeCSample(audioSample, False, False)
     C_Bridge.esper.pitchCalcFallback(cSample, global_consts.config)
-    audioSample.pitchDeltas = audioSample.pitchDeltas.nonzero()
-    audioSample.pitchMarkers = audioSample.pitchMarkers.nonzero()
+    audioSample.pitchDeltas = audioSample.pitchDeltas[audioSample.pitchDeltas.nonzero()].flatten()
+    audioSample.pitchMarkers = audioSample.pitchMarkers[audioSample.pitchMarkers.nonzero()].flatten()
 
 def calculatePitchFallback_legacy(audioSample:AudioSample) -> None:
     batchSize = math.floor((1. + audioSample.searchRange) * global_consts.sampleRate / audioSample.expectedPitch)

src/Backend/ESPER/SpectralCalculator.py
@@ -59,21 +59,21 @@ def calculateSpectra_legacy(audioSample:AudioSample, useVariance:bool = True) ->
     audioSample = separateVoicedUnvoiced(audioSample)
     audioSample = averageSpectra(audioSample, useVariance)
 
-def processWorker(input, output, limiter, useVariance, allow_oop):
+def processWorker(input, output, useVariance, allow_oop):
     while True:
         sample = input.get()
         if sample is None:
             break
-        calculatePitch(sample, limiter)
+        calculatePitch(sample)
         calculateSpectra(sample, useVariance, allow_oop)
         output.put(sample)
 
-def asyncProcess(limiter, useVariance, allow_oop):
+def asyncProcess(useVariance, allow_oop):
     inputQueue = torch.multiprocessing.Queue()
     outputQueue = torch.multiprocessing.Queue()
     processes = []
     for _ in range(torch.multiprocessing.cpu_count()):
-        p = torch.multiprocessing.Process(target = processWorker, args = (inputQueue, outputQueue, limiter, useVariance, allow_oop))
+        p = torch.multiprocessing.Process(target = processWorker, args = (inputQueue, outputQueue, useVariance, allow_oop))
         p.start()
         processes.append(p)
     return inputQueue, outputQueue, processes